133b6c1b13ea65dea6b7fd5c727fa0dc9905a59f
sreevenkateshr <sreevenkateshr@gmail.com>
2025-10-31 12:46:24 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-// app/layout.tsx
 "use client";
 
+import "./globals.css";
+import type { ReactNode } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import "./globals.css";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-white text-gray-900">
